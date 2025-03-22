f142bbc6b288622cbf90de6368f1fdb7dc9e8975
Rob Weber <rob.weber87@gmail.com>
2025-03-22 15:13:51 +0100

    add test coverage for API

---
+++ tests/Unit/PatientControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Assignment;
+use App\Models\Patient;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PatientControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_return_unassigned_patients()
+    {
+        $unassignedPatients = Patient::factory(3)->create();
+        $assignedPatient = Patient::factory()->create();
+        Assignment::factory()->create(['patient_id' => $assignedPatient->id]);
+
+        $response = $this->getJson('/api/unassigned-patients');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(3);
+    }
+}
